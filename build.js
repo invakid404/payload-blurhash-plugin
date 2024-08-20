dead8fb76e875b63d1871974ab461e84768fa161
invakid404 <invakid404@riseup.net>
2024-08-20 19:59:37 +0300

    chore: convert to ESM
    Payload 3.x appears to use ESM, so it's best if this is ESM as well.

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const { spawnSync } = require('child_process');
+import fs from 'fs/promises';
+import path from 'path';
+import ncc from '@vercel/ncc';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const bundledDependencies = new Set([
   // Thumbhash is an ESM module, so we need to bundle it
@@ -8,7 +13,7 @@ const bundledDependencies = new Set([
 ]);
 
 const packageJSON = JSON.parse(
-  fs.readFileSync('package.json', { encoding: 'utf-8' }),
+  await fs.readFile('package.json', { encoding: 'utf-8' }),
 );
 
 const externals = [
@@ -16,7 +21,7 @@ const externals = [
   ...Object.keys(packageJSON.peerDependencies),
 ].filter((dependency) => !bundledDependencies.has(dependency));
 
-require('@vercel/ncc')(path.resolve(__dirname, './src/index.ts'), {
+await ncc(path.resolve(__dirname, './src/index.ts'), {
   // provide a custom cache path or disable caching
   cache: false,
   externals,
@@ -31,51 +36,51 @@ require('@vercel/ncc')(path.resolve(__dirname, './src/index.ts'), {
   sourceMapRegister: true, // default
   watch: false, // default
   license: '', // default does not generate a license file
-  target: 'es2015', // default
+  target: 'es2020', // default
   v8cache: false, // default
   quiet: false, // default
-  debugLog: true // default
-}).then(({ code, map, assets }) => {
+  debugLog: true, // default
+}).then(async ({ code, map, assets }) => {
   // Assets is an object of asset file names to { source, permissions, symlinks }
   // expected relative to the output code (if any)
 
-  Object.entries(assets).forEach(([fileName, {source, permissions}]) => {
-    const destFileName = path.resolve('./lib/', fileName)
-    fs.mkdirSync(path.dirname(destFileName), {recursive: true})
-    fs.writeFileSync(destFileName, source, {
-      mode: permissions
+  for (const [fileName, { source, permissions }] of Object.entries(assets)) {
+    const destFileName = path.resolve(__dirname, './lib/', fileName);
+    await fs.mkdir(path.dirname(destFileName), { recursive: true });
+    await fs.writeFile(destFileName, source, {
+      mode: permissions,
     });
-  })
+  }
 
-  fs.writeFileSync('./lib/index.js', code);
-})
+  await fs.writeFile(path.resolve(__dirname, './lib/index.js'), code);
+});
 
+const newPackageJSON = {
+  ...packageJSON,
+  scripts: {},
+  main: 'index.js',
+  types: 'index.d.ts',
+  private: false,
+  files: ['**/*'],
+  dependencies: externals.reduce((acc, dependency) => {
+    if (!(dependency in packageJSON.dependencies)) {
+      return acc;
+    }
 
-fs.writeFileSync(
-  './lib/package.json',
-  JSON.stringify(
-    {
-      ...packageJSON,
-      scripts: {},
-      main: 'index.js',
-      types: "index.d.ts",
-      private: false,
-      files: ['**/*'],
-      dependencies: externals.reduce((acc, dependency) => {
-        if (!(dependency in packageJSON.dependencies)) {
-          return acc;
-        }
+    return {
+      ...acc,
+      [dependency]: packageJSON.dependencies[dependency],
+    };
+  }, {}),
+};
 
-        return {
-          ...acc,
-          [dependency]: packageJSON.dependencies[dependency],
-        };
-      }, {}),
-    },
-    null,
-    2,
-  ),
+await fs.writeFile(
+  path.resolve(__dirname, 'lib/package.json'),
+  JSON.stringify(newPackageJSON, null, 2),
   'utf-8',
 );
 
-fs.copyFileSync("README.md", "lib/README.md")
+await fs.copyFile(
+  path.resolve(__dirname, 'README.md'),
+  path.resolve(__dirname, 'lib/README.md'),
+);
